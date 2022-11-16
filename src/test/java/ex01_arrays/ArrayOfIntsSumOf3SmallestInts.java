6f00b426db78e8dde8bff2599ecf7f03957f4c38
Felice La Rizza <larizza.felice@gmail.com>
2022-11-16 19:49:37 +0100

    3.3 Exercise: sumOfEven

---
+++ src/test/java/ex01_arrays/ArrayOfIntsSumOf3SmallestInts.java
@@ -0,0 +1,35 @@
+package ex01_arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ArrayOfIntsSumOf3SmallestInts {
+
+    private final ArrayOfInts arrayOfInts = new ArrayOfInts();
+
+    @Test
+    void happyflow() {
+        int result = arrayOfInts.sumOf3SmallestInts(new int[]{1, 2, 3, 4});
+        assertEquals(6, result);
+    }
+
+    @Test
+    void paranoia() {
+        int result = arrayOfInts.sumOf3SmallestInts(new int[]{2, 7});
+        assertEquals(9, result);
+    }
+
+    @Test
+    void emptyArray() {
+        int result = arrayOfInts.sumOf3SmallestInts(new int[]{});
+        assertEquals(0, result);
+    }
+
+    @Test
+    void nullArray() {
+        int result = arrayOfInts.sumOf3SmallestInts(null);
+        assertEquals(0, result);
+    }
+
+}
---
+++ src/test/java/ex01_arrays/ArrayOfIntsSumOfEven.java
@@ -0,0 +1,35 @@
+package ex01_arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ArrayOfIntsSumOfEven {
+
+    private final ArrayOfInts arrayOfInts = new ArrayOfInts();
+
+    @Test
+    void happyflow() {
+        int result = arrayOfInts.sumOfEven(new int[]{1, 2, 3, 4});
+        assertEquals(6, result);
+    }
+
+    @Test
+    void paranoia() {
+        int result = arrayOfInts.sumOfEven(new int[]{2, 7});
+        assertEquals(2, result);
+    }
+
+    @Test
+    void emptyArray() {
+        int result = arrayOfInts.sumOfEven(new int[]{});
+        assertEquals(0, result);
+    }
+
+    @Test
+    void nullArray() {
+        int result = arrayOfInts.sumOfEven(null);
+        assertEquals(0, result);
+    }
+
+}
